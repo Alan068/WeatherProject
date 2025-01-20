8c9174fe4d6100f1716b708bda5008c440404f08
Alan068 <alanshibu.19@gmail.com>
2025-01-20 16:53:33 +0530

    Initial commit of WeatherPy project

---
+++ main.py
@@ -0,0 +1,42 @@
+import requests
+from dotenv import load_dotenv
+import os
+
+def configure():
+    load_dotenv()
+
+configure()
+
+BASE_URL = "https://api.openweathermap.org/data/2.5/weather"
+API_KEY = os.getenv('API_KEY')
+CITY = "New Delhi"
+
+if not API_KEY:
+    raise ValueError("API_KEY is missing. Check .env file.")
+
+
+url = f"{BASE_URL}?appid={API_KEY}&q={CITY}"
+response = requests.get(url).json()
+
+
+
+
+def kelvin_to_celsius(kelvin):
+    celsius = kelvin - 273.15
+    return celsius
+
+# Extract weather data
+temp_kelvin = response['main']['temp']
+temp_celsius = kelvin_to_celsius(temp_kelvin)
+
+feels_like_kelvin = response['main']['feels_like']
+feels_like_celsius = kelvin_to_celsius(feels_like_kelvin)
+
+humidity = response['main']['humidity']
+description = response['weather'][0]['description']
+
+# Print results
+print(f"Temperature in {CITY}: {temp_celsius:.2f}°C")
+print(f"Temperature in {CITY} feels like: {feels_like_celsius:.2f}°C")
+print(f"Humidity in {CITY}: {humidity}%")
+print(f"Weather description: {description}")
